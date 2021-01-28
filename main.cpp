db205cdb32ae0b5e57bedb720179776352f1af15
ACHarrison32 <AndrewHarrisonC@gmail.com>
2021-01-28 20:02:43 +0000

    Made a switch statement for a starting screen that prompts the user to go to certain areas

--- main.cpp
+++ main.cpp
@@ -36,6 +36,7 @@ int main()
   int numGames;
   string nameGame;
   int index = 0;
+  char selection;
   
   // Reads number of games in input file
   read >> numGames;
@@ -49,54 +50,79 @@ int main()
         read >> library[i].Year;
   }
 
-  // Outputs example and rule for search
-  cout << '\n';
-  cout << "Instructions: " << '\n';
-  cout << "Use _ as spaces when looking up games" << '\n';
-  cout << "Make sure to use capital letters where they are needed" << '\n';
-  cout << "EXAMPLE: Hyrule_Warriors_Age_of_Calamity" << '\n';
-  cout << "At the end of search type NEW or new for a new search" << '\n';
-  cout << "If you want to stop the program just type END to stop program" << "\n\n";
-
-  // Enter the name for program to find and display information
-  cout << "What game would you like? ";
-  cin >> nameGame;
-  cout << '\n';
-
-  // Sentinel loop where you type END to stop program 
-  while (nameGame != "END")
+  while (selection != 4)
   {
-    // loop to find game and display information
-    for (int i = 0; i < numGames; i++)
+    cout << "Please input number for what selection you would like" << '\n';
+    cout << "1. Instructions" << '\n';
+    cout << "2. Example" << '\n';
+    cout << "3. Search Engine" << '\n';
+    cout << "Selction: ";
+    cin >> selection;
+    cout << '\n';
+    // switch statment for multiple selections
+    switch (selection)
     {
-      // if statement for displaying games
-      if(library[i].Title == nameGame)
+      case '1':
+        cout << "INSTRUCTIONS" << '\n';
+        cout << "Use _ as spaces when looking up games" << '\n';
+        cout << "Make sure to use capital letters where they are needed" << '\n';
+        cout << "At the end of search type NEW or new for a new search" << '\n';
+        cout << "If you want to stop the program just type END to stop program" << '\n';
+        cout << "----------------------------------------------------------------" << '\n';
+        break;
+      
+      case '2':
+        cout << "EXAMPLE" << '\n';
+        cout << "Please write the name of the game like this: " << '\n';
+        cout << "Hyrule_Warriors_Age_of_Calamity" << '\n';
+        cout << "----------------------------------------------------------------" << '\n';
+        break;
+
+      case '3':
+        // Enter the name for program to find and display information
+      cout << "What game would you like? ";
+      cin >> nameGame;
+      cout << '\n';
+
+      // Sentinel loop where you type END to stop program 
+      while (nameGame != "END")
       {
-        index = i;
+        // loop to find game and display information
+        for (int i = 0; i < numGames; i++)
+        {
+          // if statement for displaying games
+          if(library[i].Title == nameGame)
+          {
+            index = i;
 
-        // Outputs information
-        cout << left << setw(17) <<  "Title: " << library[index].Title 
-             << left << setw(18) <<"\nPrice: " <<library[index].Price 
-             << left << setw(18) <<"\nRating:  " <<library[index].Rating 
-             << left << setw(18) <<"\nRelease Date:  " <<library[index].Year;
-        
-        // Makes spaces for multiple searches
-        string NEW;
-        cout << "\n\n" << "Type NEW for the new search" << '\n';
-        cin >> NEW;
-        cout << string(75, '\n');
-        // Repeats question for new search
-        cout << "\n" << "What game are you looking for?" << '\n';
-        cout << "Type END to stop program" << '\n';
-        cin >> nameGame;
+            // Outputs information
+            cout << left << setw(17) <<  "Title: " << library[index].Title 
+                << left << setw(18) <<"\nPrice: " <<library[index].Price 
+                << left << setw(18) <<"\nRating:  " <<library[index].Rating 
+                << left << setw(18) <<"\nRelease Date:  " <<library[index].Year;
+            
+            // Makes spaces for multiple searches
+            string NEW;
+            cout << "\n\n" << "Type NEW for the new search" << '\n';
+            cin >> NEW;
+            cout << string(75, '\n');
+            // Repeats question for new search
+            cout << "\n" << "What game are you looking for?" << '\n';
+            cout << "Type END to stop program" << '\n';
+            cin >> nameGame;
+          }
+        }
       }
+      // Program ending statement
+      cout << "Thank you, have a nice day :)";
+      //Closes if you type END
+      read.close();
+      return(0);
     }
-  }
-  // Program ending statement
-  cout << "Thank you, have a nice day :)";
 
-  // closes input file and returns
-  read.close();
-  return(0);
+  }
+    // closes input file and returns
+    read.close();
+    return(0);
 }
 
